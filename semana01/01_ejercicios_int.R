734a1a748a260469d7530875f86bfc7aa986a90e
William Paredes <229@gruporeal.ad>
2025-02-19 16:27:49 -0600

    agregando la matriz

---
+++ semana01/01_ejercicios_int.R
@@ -0,0 +1,20 @@
+##### Ejercicios de Introduccion 
+
+library(tidyverse)
+library(dslabs)
+library(rafalib)
+library(UsingR)
+
+data("father.son")
+
+head(father.son)
+
+mean(father.son$sheight)
+
+
+mean(father.son$sheight[round(father.son$fheight) == 71])
+
+
+#RNGkind()
+
+
