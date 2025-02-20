62e668837c4b37fe8e437901700ebe789a5c5ab0
William Paredes <229@gruporeal.ad>
2025-02-20 16:14:39 -0600

    Agregando cambios

--- semana01/01_ejercicios_int.R
+++ semana01/01_ejercicios_int.R
@@ -15,3 +15,27 @@ mean(father.son$sheight[round(father.son$fheight) == 71])
 #RNGkind()
 
 
+## Matrix notation excercises
+
+X <- matrix(1:1000,100,10)
+X[25,3]
+
+
+x <- 1:10
+
+
+
+matr_exemple <- cbind(X1=x, X2 = 2*x, X3 = 3*x, X4 = 4*x, X5 = 5*x)
+
+sum(matr_exemple[7,])
+
+
+matrix(1:60, 20, 3, byrow = TRUE)
+
+x <- 11:20
+rbind(x,2*x, 3*x)
+
+x <- 1:40
+matrix(3*x, 20, 2)
+
+seq(10,1,-2)
---
+++ semana01/02_Matrix_Operations.R
@@ -0,0 +1,31 @@
+#### Matrix Operations
+
+library(tidyverse)
+library(dslabs)
+library(rafalib)
+
+
+X <- matrix(1:12,4,3)
+X
+a <- 2
+a*X
+
+
+X <- matrix(1:12,4,3)
+X
+t(X)
+
+
+X  <- matrix(c(1,3,2,1,-2,1,1,1,-1),3,3)
+abc <- c(3,2,1) #use as an example
+rbind( sum(X[1,]*abc), sum(X[2,]*abc), sum(X[3,]*abc))
+
+X%*%abc
+
+n <- 5 #pick dimensions
+diag(n)
+
+
+X <- matrix(c(1,3,2,1,-2,1,1,1,-1),3,3)
+y <- matrix(c(6,2,1),3,1)
+solve(X)%*%y #equivalent to solve(X,y)
