27816f8332ead0424e200a250b3c2ddddcb902aa
William Paredes <229@gruporeal.ad>
2025-03-06 07:16:54 -0600

    Aregando los temas

--- Semana03/11_lineal_models_as_matrix.R
+++ Semana03/11_lineal_models_as_matrix.R
@@ -12,3 +12,58 @@ system.time({
   sum((y-x %*% beta)^2)
 })
 
+# Lineal algebra
+
+x <- c(1,1,2,2)
+f <- formula(~x)
+f
+
+model.matrix(f)
+
+class(x)
+
+group <- factor( c(1,1,2,2) )
+model.matrix(~ group)
+
+model.matrix(formula(~ group))
+
+group <- c(1,1,2,2)
+model.matrix(~ group)
+
+group <- factor(c("control","control","highfat","highfat"))
+model.matrix(~ group)
+
+group <- factor(c(1,1,2,2,3,3))
+model.matrix(~ group)
+
+
+group <- factor(c(1,1,2,2,3,3))
+model.matrix(~ group + 0)
+
+diet <- factor(c(1,1,1,1,2,2,2,2))
+sex <- factor(c("f","f","m","m","f","f","m","m"))
+table(diet,sex)
+
+diet <- factor(c(1,1,1,1,2,2,2,2))
+sex <- factor(c("f","f","m","m","f","f","m","m"))
+model.matrix(~ diet + sex)
+
+model.matrix(~ diet + sex + diet:sex)
+
+model.matrix(~ diet*sex)
+
+
+group <- factor(c(1,1,2,2))
+group <- relevel(group, "2")
+model.matrix(~ group)
+
+
+group <- factor(group, levels=c("1","2"))
+model.matrix(~ group)
+
+group <- 1:4
+model.matrix(~ group, data=data.frame(group=5:8))
+
+tt <- seq(0,3.4,len=4) 
+model.matrix(~ tt + I(tt^2))
+
